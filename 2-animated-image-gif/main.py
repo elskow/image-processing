4d54c2f6deb3e69b67d4d5033c98d6a998152c50
Helmy LuqmanulHakim <elskow@outlook.com>
2024-02-24 20:58:22 +0700

    [task 2]: more proper with report on latex format

--- 2-animated-image-gif/main.py
+++ 2-animated-image-gif/main.py
@@ -3,23 +3,15 @@ import os
 
 IMAGE_PATH = "./static/screaming-cat.gif"
 
+if not os.path.exists("./static/extracted_frames"):
+    os.mkdir("./static/extracted_frames")
+
 if __name__ == "__main__":
     gif_image = Image.open(IMAGE_PATH)
-
-    print(f"Image format: {gif_image.format}")
-    print(f"Image mode: {gif_image.mode}")
-    print(f"Is animated: {gif_image.is_animated}")
-    print(f"Number of frames: {gif_image.n_frames}")
-    print(f"Duration: {gif_image.info['duration']}")
-    print(f"Loop: {gif_image.info['loop']}")
-    print(f"Extension: {gif_image.info['extension']}")
-    print(f"Size: {gif_image.size}")
-
-
-    for frame in range(gif_image.n_frames):
-        gif_image.seek(frame)
-        try:
-            gif_image.save(f"./output/frame_{frame}.png")
-        except:
-            os.mkdir("./output")
-            gif_image.save(f"./output/frame_{frame}.png")
+    print(f"Jumlah frame: {gif_image.n_frames}")
+    for i in range(gif_image.n_frames):
+        gif_image.seek(i)
+        frame = gif_image.convert("L")
+        frame.save(f"./static/extracted_frames/frame_{i}.png")
+    frames = [Image.open(f"./static/extracted_frames/frame_{i}.png") for i in range(gif_image.n_frames)]
+    frames[0].save("./static/new_gif.gif", save_all=True, append_images=frames[1:], loop=0, duration=gif_image.info["duration"])
